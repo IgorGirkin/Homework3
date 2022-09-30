76c1b240c26d32b83ab285a260b1f4aaaaa4bfb8
grisha <norsulfasol@gmail.com>
2022-10-01 02:00:05 +0300

    ДЗ-3 Задача 5

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int machinePerformance=8;
-        int twentyMinutes=machinePerformance*20;
-        int day=machinePerformance*(24*60);
-        int threeDays=machinePerformance*(24*60*3);
-        int month=machinePerformance*(24*60*31);
-        System.out.println("За двадцать минут машина произвела бутылок "+twentyMinutes +" штук");
-        System.out.println("За сутки машина произвела бутылок " +day+" штук");
-        System.out.println("За три дня машина произвела бутылок " +threeDays+ "штук");
-        System.out.println("За месяц машина произвела бутылок " +month+ "штук");
+        byte allPaint = 120;
+        byte white=2;
+        byte brown=4;
+        int totalPremises=allPaint/(brown+white);
+        int allWhite=totalPremises*white;
+        int allBrown=totalPremises*brown;
+        System.out.println("В школе, где "+totalPremises+ " классов, нужно "+allWhite+" банок белой краски" +
+                " и "+allBrown+" банок коричневой краски.");
 
     }
 }
