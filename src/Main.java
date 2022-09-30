4a8c47a33f1ee6f36c15bb1e60d0527213b333f3
grisha <norsulfasol@gmail.com>
2022-10-01 00:48:18 +0300

    ДЗ-3 Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int wholeNumbers=22;
-        short twoBytes=12;
-        long bigLong=327L;
-        float floatingPoint=8.36f;
-        double eightBytes =56.254;
-        System.out.println("Задание1");
-        System.out.println("Значение переменной wholeNumbers с типом int равно " + wholeNumbers);
-        System.out.println("Значение переменной twoBytes с типом int равно " + twoBytes);
-        System.out.println("Значение переменной bigLong с типом int равно " + bigLong);
-        System.out.println("Значение переменной floatingPoint с типом int равно " + floatingPoint);
-        System.out.println("Значение переменной eightBytes с типом int равно " + eightBytes);
+        int lyudmilaPavlovna=23;
+        int annaSergeyevna=27;
+        int ekaterinaAndreevna=30;
+        int paper = 480;
+        int getStudent=paper/(lyudmilaPavlovna+annaSergeyevna+ekaterinaAndreevna);
+        System.out.println("На каждого ученика рассчитано " +getStudent+" листов бумаги");
+
     }
 }
