ccd4b87888ff01eaa65203d215d89a6b260cb485
grisha <norsulfasol@gmail.com>
2022-10-01 19:02:40 +0300

    ДЗ-3 Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int machinePerformance=8;
+        int bottles = 16;
+        int minutes=2;
+        int machinePerformance=bottles/minutes;
         int twentyMinutes=machinePerformance*20;
-        int day=machinePerformance*(24*60);
-        int threeDays=machinePerformance*(24*60*3);
-        int month=machinePerformance*(24*60*31);
+        int minetdey=24*60;
+        int day=machinePerformance*minetdey;
+        int threeDays=machinePerformance*minetdey*3;
+        int month=machinePerformance*minetdey*31;
         System.out.println("За двадцать минут машина произвела бутылок "+twentyMinutes +" штук");
         System.out.println("За сутки машина произвела бутылок " +day+" штук");
-        System.out.println("За три дня машина произвела бутылок " +threeDays+ "штук");
-        System.out.println("За месяц машина произвела бутылок " +month+ "штук");
+        System.out.println("За три дня машина произвела бутылок " +threeDays+ " штук");
+        System.out.println("За месяц машина произвела бутылок " +month+ " штук");
 
     }
 }
