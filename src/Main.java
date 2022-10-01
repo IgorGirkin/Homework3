5ada3b366e0ff0fa101b8d0ddb2607c6d6244964
grisha <norsulfasol@gmail.com>
2022-10-01 17:52:16 +0300

    ДЗ-3 Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int wholeNumbers=22;
+        byte garaz = 120;
         short twoBytes=12;
         long bigLong=327L;
         float floatingPoint=8.36f;
         double eightBytes =56.254;
         System.out.println("Задание1");
         System.out.println("Значение переменной wholeNumbers с типом int равно " + wholeNumbers);
+        System.out.println("Значение переменной garaz с типом int равно " + garaz);
         System.out.println("Значение переменной twoBytes с типом int равно " + twoBytes);
         System.out.println("Значение переменной bigLong с типом int равно " + bigLong);
         System.out.println("Значение переменной floatingPoint с типом int равно " + floatingPoint);
