03eb785a90af19e5950ceb4286f1176392dd1989
grisha <norsulfasol@gmail.com>
2022-10-01 17:08:18 +0300

    ДЗ-3 Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int needReset= 7;
-        needReset*=1000;
-        int lightWorkout =250;
-        int hardWorkout =500;
-        int averageWorkout=(hardWorkout+lightWorkout)/2;
-        System.out.println("Длительность тренеровки для похудяня (лекий режим) - спортсмен теряет "
-                +lightWorkout+ " г. в день, составит "+needReset/lightWorkout+" дней.");
-        System.out.println("Длительность тренеровки для похудяня (тяжелый режим) - спортсмен теряет "
-                +hardWorkout+ " г. в день, составит "+needReset/hardWorkout+" дней.");
-        System.out.println("В среднем, чтобы добиться результата похудения потребуется "+
-                (needReset%averageWorkout/lightWorkout+needReset/averageWorkout)+" дней.");
+        int masha= 67760;
+        float motivationMasha=masha+masha*0.1F;
+        int denchik =83690;
+        float motivationDenchik=denchik+denchik*0.1F;
+        int kris =76230;
+        float motivatiomKris=kris+kris*0.1F;
+        System.out.println("Маша теперь получает "+ motivationMasha+" рублей в месяц. Годовой доход " +
+                "вырос на " +(motivationMasha-masha)*12+" рублей.");
+        System.out.println("Денис теперь получает "+ motivationDenchik+" рублей в месяц. Годовой доход " +
+                "вырос на " +(motivationDenchik-denchik)*12+" рублей.");
+        System.out.println("Кристина теперь получает "+ motivatiomKris+" рублей в месяц. Годовой доход " +
+                "вырос на " +(motivatiomKris-kris)*12+" рублей.");
 
     }
 }
