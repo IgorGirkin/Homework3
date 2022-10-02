e5bdd0634379905d4d378c83fb2b92e9eb3f65f5
grisha <norsulfasol@gmail.com>
2022-10-02 20:51:24 +0300

    ДЗ-3 Задача 8

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,12 @@ public class Main {
         needReset*=1000;
         int lightWorkout =250;
         int hardWorkout =500;
-        int averageWorkout=(hardWorkout+lightWorkout)/2;
         System.out.println("Длительность тренеровки для похудяня (лекий режим) - спортсмен теряет "
                 +lightWorkout+ " г. в день, составит "+needReset/lightWorkout+" дней.");
         System.out.println("Длительность тренеровки для похудяня (тяжелый режим) - спортсмен теряет "
                 +hardWorkout+ " г. в день, составит "+needReset/hardWorkout+" дней.");
         System.out.println("В среднем, чтобы добиться результата похудения потребуется "+
-                (needReset%averageWorkout/lightWorkout+needReset/averageWorkout)+" дней.");
+                (needReset/lightWorkout+needReset/hardWorkout)/2+" дней.");
 
     }
 }
